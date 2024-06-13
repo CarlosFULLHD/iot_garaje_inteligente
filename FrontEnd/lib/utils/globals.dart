33043b4dc65fe2eb22d7691e5a67a1bb054245a7
Natalia <bgutisol2019@gmail.com>
2024-06-13 01:34:52 -0400

    mi entorno de desarrollo

--- FrontEnd/lib/utils/globals.dart
+++ FrontEnd/lib/utils/globals.dart
@@ -2,5 +2,5 @@ class Globals{
   // static String url = 'http://192.168.1.5:8080/api/v1';
   // static String url = 'http://172.18.10.10:8080/api/v1';
   //static String url = 'http://10.0.2.2:8080/api/v1';
-  static String url = 'http://172.18.0.186:8080/api/v1';
+  static String url = 'http://192.168.100.115:8080/api/v1';
 }
