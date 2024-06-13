7d68d6e5f11560f675e8edb6fe10bcb2ee1014d5
Natalia <bgutisol2019@gmail.com>
2024-06-13 07:23:57 -0400

    horas actualizadas

--- FrontEnd/lib/views/dashboard_admin_view.dart
+++ FrontEnd/lib/views/dashboard_admin_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:smartpark/providers/admin_dashboard_provider.dart';
 import 'package:smartpark/models/spot_stats_model.dart';
 import 'package:smartpark/models/parking_model.dart';
@@ -52,16 +51,7 @@ class _DashboardAdminViewState extends State<DashboardAdminView> {
                     buildExpansionPanels(parkingStats),
                     SizedBox(height: 16),
                     Center(child: Text('Gráfica General de Reservas por Spot', style: Theme.of(context).textTheme.headlineSmall)),
-                    Container(
-                      height: 300,
-                      child: GeneralSpotUsageBarChart(createGeneralSpotUsageBarData(parkingStats)),
-                    ),
-                    SizedBox(height: 16),
-                    Center(child: Text('Horas Ocupadas por Spot', style: Theme.of(context).textTheme.headlineSmall)),
-                    Container(
-                      height: 300,
-                      child: GeneralSpotHoursOccupiedChart(createGeneralSpotHoursOccupiedBarData(parkingStats)),
-                    ),
+                    // Aquí puedes agregar una gráfica general si es necesario
                   ],
                 ),
               ),
@@ -82,14 +72,19 @@ class _DashboardAdminViewState extends State<DashboardAdminView> {
             children: parking.spotUsageStats.map((SpotStatsModel spotStats) {
               return ListTile(
                 title: Text('Spot: ${parking.spotUsageStats.indexOf(spotStats) + 1}'),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                subtitle: Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  alignment: WrapAlignment.center,
                   children: [
-                    Text('Total Reservas: ${spotStats.totalReservations}'),
-                    Text('Total Horas Ocupadas: ${spotStats.totalHoursOccupied}'),
-                    SizedBox(
-                      height: 200,
-                      child: SpotUsageBarChart(createSpotUsageBarData([spotStats])),
+                    buildInfoCard(
+                      context,
+                      'Spot: ${parking.spotUsageStats.indexOf(spotStats) + 1}',
+                      spotStats.totalReservations.toString(),
+                      'Reservas',
+                      formatHours(spotStats.totalHoursOccupied),
+                      'Total Horas Ocupadas',
+                      Colors.blue,
                     ),
                   ],
                 ),
@@ -101,147 +96,36 @@ class _DashboardAdminViewState extends State<DashboardAdminView> {
     );
   }
 
-  List<charts.Series<SpotUsage, String>> createSpotUsageBarData(List<SpotStatsModel> spotStats) {
-    final data = spotStats.map((stats) {
-      return SpotUsage(
-        'Spot ${spotStats.indexOf(stats) + 1}',
-        stats.totalReservations,
-        stats.totalHoursOccupied,
-      );
-    }).toList();
-
-    return [
-      charts.Series<SpotUsage, String>(
-        id: 'Total Reservas',
-        domainFn: (SpotUsage usage, _) => usage.spot,
-        measureFn: (SpotUsage usage, _) => usage.totalReservations,
-        data: data,
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-      ),
-      charts.Series<SpotUsage, String>(
-        id: 'Total Horas Ocupadas',
-        domainFn: (SpotUsage usage, _) => usage.spot,
-        measureFn: (SpotUsage usage, _) => usage.totalHoursOccupied,
-        data: data,
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-      )..setAttribute(charts.rendererIdKey, 'customBar'),
-    ];
-  }
-
-  List<charts.Series<GeneralSpotUsage, String>> createGeneralSpotUsageBarData(List<ParkingStatsModel> parkingStats) {
-    final data = parkingStats.expand((parking) {
-      return parking.spotUsageStats.map((stats) {
-        return GeneralSpotUsage(
-          'Spot ${parking.spotUsageStats.indexOf(stats) + 1}',
-          stats.totalReservations,
-        );
-      }).toList();
-    }).toList();
-
-    return [
-      charts.Series<GeneralSpotUsage, String>(
-        id: 'Total Reservas',
-        domainFn: (GeneralSpotUsage usage, _) => usage.spot,
-        measureFn: (GeneralSpotUsage usage, _) => usage.totalReservations,
-        data: data,
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-      ),
-    ];
-  }
-
-  List<charts.Series<GeneralSpotHoursOccupied, String>> createGeneralSpotHoursOccupiedBarData(List<ParkingStatsModel> parkingStats) {
-    final data = parkingStats.expand((parking) {
-      return parking.spotUsageStats.map((stats) {
-        return GeneralSpotHoursOccupied(
-          'Spot ${parking.spotUsageStats.indexOf(stats) + 1}',
-          stats.totalHoursOccupied,
-        );
-      }).toList();
-    }).toList();
-
-    return [
-      charts.Series<GeneralSpotHoursOccupied, String>(
-        id: 'Total Horas Ocupadas',
-        domainFn: (GeneralSpotHoursOccupied usage, _) => usage.spot,
-        measureFn: (GeneralSpotHoursOccupied usage, _) => usage.totalHoursOccupied,
-        data: data,
-        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-      ),
-    ];
-  }
-}
-
-class SpotUsageBarChart extends StatelessWidget {
-  final List<charts.Series<dynamic, String>> seriesList;
-  final bool animate;
-
-  SpotUsageBarChart(this.seriesList, {this.animate = false});
-
-  @override
-  Widget build(BuildContext context) {
-    return charts.BarChart(
-      seriesList,
-      animate: animate,
-      barGroupingType: charts.BarGroupingType.grouped,
-      customSeriesRenderers: [
-        charts.BarTargetLineRendererConfig<String>(
-          customRendererId: 'customBar',
+  Widget buildInfoCard(BuildContext context, String spotLabel, String reservations, String reservationsLabel, String totalHours, String hoursLabel, Color color) {
+    return Card(
+      color: color.withOpacity(0.7),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(child: Text(spotLabel, style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.white))),
+            SizedBox(height: 8),
+            Center(child: Text(reservations, style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white))),
+            Center(child: Text(reservationsLabel, style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.white))),
+            SizedBox(height: 8),
+            Center(child: Text(totalHours, style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white))),
+            Center(child: Text(hoursLabel, style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.white))),
+          ],
         ),
-      ],
+      ),
     );
   }
-}
 
-class GeneralSpotUsageBarChart extends StatelessWidget {
-  final List<charts.Series<dynamic, String>> seriesList;
-  final bool animate;
-
-  GeneralSpotUsageBarChart(this.seriesList, {this.animate = false});
-
-  @override
-  Widget build(BuildContext context) {
-    return charts.BarChart(
-      seriesList,
-      animate: animate,
-      barGroupingType: charts.BarGroupingType.grouped,
-    );
+  String formatHours(double hours) {
+    // Omitir la parte desde que tiene -e para adelante
+    final String hoursString = hours.toString().split('e')[0];
+    final double parsedHours = double.parse(hoursString);
+    print('Total hours: $parsedHours'); // Línea añadida para imprimir el valor de horas sin formatear
+    final int totalSeconds = (parsedHours * 3600).round();
+    final int h = totalSeconds ~/ 3600;
+    final int m = (totalSeconds % 3600) ~/ 60;
+    final int s = totalSeconds % 60;
+    return '${h}h ${m}m ${s}s';
   }
-}
-
-class GeneralSpotHoursOccupiedChart extends StatelessWidget {
-  final List<charts.Series<dynamic, String>> seriesList;
-  final bool animate;
-
-  GeneralSpotHoursOccupiedChart(this.seriesList, {this.animate = false});
-
-  @override
-  Widget build(BuildContext context) {
-    return charts.BarChart(
-      seriesList,
-      animate: animate,
-      barGroupingType: charts.BarGroupingType.grouped,
-    );
-  }
-}
-
-class SpotUsage {
-  final String spot;
-  final int totalReservations;
-  final double totalHoursOccupied;
-
-  SpotUsage(this.spot, this.totalReservations, this.totalHoursOccupied);
-}
-
-class GeneralSpotUsage {
-  final String spot;
-  final int totalReservations;
-
-  GeneralSpotUsage(this.spot, this.totalReservations);
-}
-
-class GeneralSpotHoursOccupied {
-  final String spot;
-  final double totalHoursOccupied;
-
-  GeneralSpotHoursOccupied(this.spot, this.totalHoursOccupied);
 }
