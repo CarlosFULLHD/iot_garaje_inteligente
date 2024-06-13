f3ba7fff4883d21b7bdd10efada0bc3363c9b19e
Carlos Nina Reynaga <119177486+CarlosFULLHD@users.noreply.github.com>
2024-06-13 00:12:54 -0400

    Fix: Update SpotDao to order by asc

--- backend/src/main/java/bo/edu/ucb/smartpark/Smart/Park/UCB/dao/SpotDao.java
+++ backend/src/main/java/bo/edu/ucb/smartpark/Smart/Park/UCB/dao/SpotDao.java
@@ -3,13 +3,11 @@ package bo.edu.ucb.smartpark.Smart.Park.UCB.dao;
 import bo.edu.ucb.smartpark.Smart.Park.UCB.Entity.SpotEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
 public interface SpotDao extends JpaRepository<SpotEntity, Long> {
 
-
-    List<SpotEntity> findByParkingEntity_IdPar(Long parkingId);
+    // Agregar ordenación por número de espacio en orden ascendente
+    List<SpotEntity> findByParkingEntity_IdParOrderBySpotNumberAsc(Long parkingId);
 }
